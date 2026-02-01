e0264dc29082488939422fc79f71905427c48c45
GGang <eun1018bee@naver.com>
2026-02-01 12:15:58 +0900

    주석 제거

--- src/utils/composeImage.ts
+++ src/utils/composeImage.ts
@@ -5,16 +5,13 @@ export function composeImage(
     height: number
 ): HTMLCanvasElement {
 
-    console.log("composeImage called"); 
     const canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
+
     const ctx = canvas.getContext("2d")!;
-
-    console.log("Composing images with size:", width, height);
-
+    
     ctx.drawImage(userImage, 0, 0, width, height);
-
     ctx.drawImage(frameImage, 0, 0, width, height);
 
     return canvas;
